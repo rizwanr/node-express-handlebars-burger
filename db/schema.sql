882c001ad71bd093d8807bbfcfc78326cf99ebf8
Rizwan <rizwanrensa@gmail.com>
2019-09-08 13:16:03 -0400

    added handlebars files

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
-###Schema
-CREATE DATABASE burgers_db;
-USE burgers_db;
-CREATE TABLE burgers
-(
-  id int NOT NULL
-  AUTO_INCREMENT,
-	name varchar
-  (255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY
-  (id)
-);
+-- ###Schema
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+-- CREATE TABLE burgers
+-- (
+--   id int NOT NULL
+--   AUTO_INCREMENT,
+-- 	name varchar
+--   (255) NOT NULL,
+-- 	devoured BOOLEAN DEFAULT false,
+-- 	PRIMARY KEY
+--   (id)
+-- );
