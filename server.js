c1ca25809c549b493bfd96fd700f2d084efc1ae3
Rizwan <rizwanrensa@gmail.com>
2019-10-12 11:09:56 -0400

    fix issues for serving public folder

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+
+const path = require('path');
+
 var express = require("express");
-
-
+var exphbs = require("express-handlebars");
 
 var app = express();
-var path = require('path')
 // Serve static content for the app from the "public" directory in the application directory.pp.use(express.static("public"));
 app.use(express.static("public"));
 
@@ -13,17 +14,27 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+
+
+
+// Make use of the body-parsers
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set static directory reference path
+app.use(express.static(path.join(__dirname, 'public'))); 
+
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({
   defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
+
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgerController.js");
-
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
